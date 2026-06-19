d387b5948596b6944939c4900cf13471c3c04759
Tony Fischetti <tony.fischetti@gmail.com>
2026-06-18 22:19:13 -0400

    added some things to the info drawer

--- app/components/InfoDrawer/InfoDrawer.tsx
+++ app/components/InfoDrawer/InfoDrawer.tsx
@@ -28,6 +28,14 @@ export const SettingsDrawer = ({ infoOpenP, setInfoOpen }: Props) => {
           <li><a href="https://drive.google.com/file/d/1jet6ClL9aa92XC7RXsqJ6sEFBJC5sEnU/view" target="_blank">Link to user guide cheatsheet</a></li>
           <li><a href="https://docs.google.com/forms/d/e/1FAIpQLSeovdY7AoC1rUxzZgaCVqe_tBMEWdZGeB1OQyjIQm2zrlHljQ/viewform" target="_blank">Link to feedback form</a></li>
         </ul>
+        <br/>
+        <p>Resources for updating PPD:</p>
+        <ul>
+          <li> <a target="_blank" href="https://docs.google.com/document/d/10eMvUxxdROmFmeqN4i2Qyg0fNijoGnOwxmb5vFB89AQ/edit?tab=t.dolb72vuyo5c"> Link to updating protocol </a> </li>
+          <li> <a target="_blank" href="https://docs.google.com/spreadsheets/d/1qHL3zEDihrK9_-vlzBH9jXVokQ7b1wTCKyYGS0eNBmM/edit?gid=1237149006#gid=1237149006"> Link to PHG's ongoing log </a> </li>
+          <li> <a target="_blank" href="https://docs.google.com/spreadsheets/d/1WNBStf1R9ITiw8wKkNoX_kbKB5iSFHyvZ491J2h4pG8/edit?gid=1237149006#gid=1237149006"> Link to PRN's ongoing log </a> </li>
+          <li> <a target="_blank" href="https://docs.google.com/spreadsheets/d/1BEFP4ZFHNf1q70_sjdPmdLOjMXlavu27XIuD7V5jvTk/edit?gid=1327175105#gid=1327175105"> Link to constituent directory </a> </li>
+        </ul>
       </DrawerBody>
     </Drawer>
   );
